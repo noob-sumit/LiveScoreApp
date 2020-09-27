da8880d3ce7601f7cb37ad5bc13f017047f101ae
sumitIsHere <sumit.dhanbad8@gmail.com>
2020-09-27 14:06:41 +0530

    Updates

---
+++ precache-manifest.e8a16a887a8b586f59d1f3d0642936ec.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "65134ae2e2321b3bbc551f2031f9db1c",
+    "url": "/LiveScoreApp/index.html"
+  },
+  {
+    "revision": "026e5a449016c17eb889",
+    "url": "/LiveScoreApp/static/css/main.dcc5e358.chunk.css"
+  },
+  {
+    "revision": "b1c99c129418d71f912c",
+    "url": "/LiveScoreApp/static/js/2.9c6dc6bc.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/LiveScoreApp/static/js/2.9c6dc6bc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "026e5a449016c17eb889",
+    "url": "/LiveScoreApp/static/js/main.b3fbc124.chunk.js"
+  },
+  {
+    "revision": "657120db821638ff4595",
+    "url": "/LiveScoreApp/static/js/runtime-main.ca75dc7a.js"
+  }
+]);
